8dda3dd0350101c9b3a05f38c0dfffbf699e460e
Ankit Sharma <ankit.sharma09547@gmail.com>
2026-07-15 23:05:52 +0530

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          className="dark:invert bg-white"
+          src="/logo.svg"
+          alt="extrovatesolutionsllp logo"
+          width={300}
+          height={100}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Extrovatesolutionsllp comming soon... To get started.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
